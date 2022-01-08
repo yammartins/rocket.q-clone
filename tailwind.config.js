9f89b853fd10908e6032c593902a1ca5c8d97500
Yam <yamprado2010@hotmail.com>
2022-01-07 23:17:44 -0300

    palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,10 +17,34 @@ module.exports = {
       background: '#FBFCFF',
       Liblue: '#E0ECFF',
       trash: '#E83F5B',
+      icon: '#A5B0C1',
+      cancel: '#E5EAF1'
     },
     backgroundImage: {
       main: 'linear-gradient(195.96deg, #5498FF -122.01%, #428EFF -8.36%, #297FFF 98.4%)',
     },
+    fontFamily: {
+      sans: ['Open Sans', 'system-ui', 'sans-serif'],
+      poppins: ['Poppins', 'system-ui', 'sans-serif']
+
+    },
+    fontSize: {
+      title: ['28px', '42px'],
+      base: ['16px', '24px'],
+      sub: ['18px', '27px'],
+      details: ['14px', '19px'],
+      box: ['24px', '34px']
+    },
+     sansWeight: {
+       normal: 400,
+       medium: 500
+     },
+     poppinsWeight: {
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700
+     },
     extend: {},
   },
   plugins: [],
