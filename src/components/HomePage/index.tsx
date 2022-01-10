a203347ec5bcff9e02faf560b8fcd4afcd9907f6
Yam <yamprado2010@hotmail.com>
2022-01-10 15:03:52 -0300

    homepage:done

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -5,7 +5,7 @@ import Saly from '../../assets/Saly-6.svg';
 
 const Homepage: React.FC = () => (
 
-  <main className="container flex">
+  <main className="wraper container">
     <div className="picture flex">
       <div className="picture-logo">
         <img src={Logo} alt="logo da rocketq" />
@@ -19,22 +19,39 @@ const Homepage: React.FC = () => (
         <img src={Saly} alt="Mascote da rocket.q" />
       </div>
     </div>
-    <div className="homepage-main">
-      <div className="homepage-main-member">
+    <div className="homepage grid">
+      <div className="homepage-member relative">
         <h1 className="text-h1">Entre como participante</h1>
-        <form action="" method="post">
-          <input type="text" className="room" />
-          <button type="submit">
-            <LogoutIcon className="login" />
+        <form action="" method="post" className="grid">
+          <input
+            type="text"
+            className="room h-12 w-[22.7rem] p-4 text-base bg-[#FBFCFF]"
+            placeholder="Código da sala"
+          />
+          <button
+            type="submit"
+            className="button h-12 w-[22.7rem] bg-blue gap-3 pl-[6.5rem] pr-24"
+          >
+            <LogoutIcon
+              className="login w-6 h-6"
+            />
             Entrar na sala
           </button>
         </form>
       </div>
-      <div className="divider">ou</div>
-      <div className="homepage-main-host">
-        <h1 className="text-h1">Crie sua própria sala, de forma fácil</h1>
-        <button type="button">
-          <UsersIcon className="create" />
+      <div className="divider text-greyblue flex gap-[0.84rem]">
+        <div className="row w-[9.8rem] h-[1px] bg-greyblue" />
+        ou
+        <div className="row w-[9.8rem] h-[1px] bg-greyblue" />
+      </div>
+      <div className="homepage-host flex flex-col gap-6">
+        <h1 className="text-h1">
+          Crie sua própria sala, de
+          <br />
+          forma fácil
+        </h1>
+        <button type="button" className="button1 flex w-[22.7rem] h-12 gap-1 text-blue pl-32 pr-20">
+          <UsersIcon className="create w-5 h-5 text-blue" />
           Criar sala
         </button>
       </div>
